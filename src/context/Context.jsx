3fb606a1ab262ad95f6f515e049bd7bc12ba619e
amargiri1717@gmail.com <amargiri1717@gmail.com>
2026-01-18 18:05:39 +0530

    writting function to make it better and also creating better UI for the result.

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,21 +1,64 @@
-import { createContext } from "react";
+import { createContext , useState} from "react";
 import runChat  from "../config/gemini.js";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-   
-     const onSent = async (promt) => {
-         const res = await runChat(promt);
-            console.log(res);
-     }
 
-    
-    onSent("what is react.js?");
-     
+    const [input ,setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState("");
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+    const delayPara = (index, nextWord) => {
+         setTimeout(function(){
+            setResultData(prev => prev + nextWord);
+         },75*index)
+    }
+   
+     const onSent = async (prompt) => {
+
+         setResultData("");
+         setLoading(true);
+         setShowResult(true);
+         setRecentPrompt(input);
+          const response = await runChat(input);
+          console.log(response);
+          let responseArray = response.split("**");
+          let newResponse ;
+          for(let i=0; i<responseArray.length; i++){
+            if(i===0 || i%2 !== 1){
+                newResponse += responseArray[i];
+            }
+            else{
+                newResponse += `<b>${responseArray[i]}</b>`;
+            }
+          }
+
+          let newResponse2 = newResponse.split("*").join("<br/>")
+          let newResponseArray = newResponse2.split(" ");
+          for(let i=0;i<newResponseArray.length;i++){
+            const nextWord = newResponseArray[i] ;
+            delayPara(i,nextWord+" ");
+          }
+          setLoading(false);
+          setInput("");
+     }
 
 
     const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput,
 
     }
 
